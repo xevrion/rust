00beacbcb3c895262a49d953dc1078fc5c55594f
Yash Bavadiya <krbavadiya11@gmail.com>
2026-05-31 23:43:42 +0530

    chore: delete bounce crate, remove its Cargo.toml, comment example in data_types

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
-    let _guess: u32 = "42".parse().expect("Not a number!");
-}
+// fn main() {
+//     let _guess: u32 = "42".parse().expect("Not a number!");
+// }
+// if you dont add the : u32, compiler will throw an error because it needs more info
+
